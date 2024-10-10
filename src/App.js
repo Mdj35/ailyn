a724daf5b8d2c478add635785abf719b379ab1b2
Vynce <vynceian.oani@hcdc.edu.ph>
2024-10-10 09:12:21 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+  import React from 'react';
+  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+  import ChooseBranch from '../src/pages/chooseBranch';
+  import SignIn from './pages/signIn';
+  import Home from './pages/home';
+  import EditStaff from './pages/EditStaff';
+  import AddCommission from './pages/addCommission';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Routes>
+              <Route path="/" element={<ChooseBranch />} />
+              <Route path="/signIn" element={<SignIn />} />
+              <Route path="/home/*" element={<Home />} />
+              <Route path="/editstaff"element={<EditStaff/>}/>
+              <Route path="/home/commission/add"element={<AddCommission/>}/>
+            </Routes>
+          </header>
+        </div>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
